519fead749fe918a2922f1aa1945d45165856028
ibmdemo <ibmdemo@gmail.com>
2017-10-13 14:50:06 -0700

    first commit

---
+++ src/main/java/application/rest/v1/Example.java
@@ -0,0 +1,19 @@
+package application.rest.v1;
+
+import java.net.InetAddress;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+
+@Path("hello")
+public class Example {
+
+
+    @GET
+    public String greet() throws Exception{
+        return "Hello from " + InetAddress.getLocalHost().getHostName();
+    }
+
+
+}
